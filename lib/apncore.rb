78bc9275dc542ca659c59a199cdf661ce02bcf26
Jonathan del Strother <jon.delStrother@audioboo.fm>
2010-12-16 11:00:07 +0000

    Don't add methods to ruby's Hash class
    Introduces a PayloadHash subclass to handle payload methods

--- lib/apncore.rb
+++ lib/apncore.rb
@@ -1,42 +1,52 @@
 $KCODE='u' and require 'jcode' if RUBY_VERSION =~ /1.8/
 require 'json'
 
-class Hash
-  MAX_PAYLOAD_LEN = 256
+module APNs4r
 
-  # Converts hash into JSON String.
-  # When payload is too long but can be chopped, tries to cut self.[:aps][:alert].
-  # If payload still don't fit Apple's restrictions, returns nil
-  #
-  # @return [String, nil] the object converted into JSON or nil.
-  def to_payload
-    # Payload too long
-    if (to_json.length > MAX_PAYLOAD_LEN)
-      alert = self[:aps][:alert]
-      self[:aps][:alert] = ''
-      # can be chopped?
-      if (to_json.length > MAX_PAYLOAD_LEN)
-        return nil
-      else # inefficient way, but payload may be full of unicode-escaped chars, so...
-        self[:aps][:alert] = alert
-        while (self.to_json.length > MAX_PAYLOAD_LEN)
-          self[:aps][:alert].chop!
-        end
+  class PayloadHash < Hash
+    MAX_PAYLOAD_LEN = 256
+
+    def initialize(constructor = {})
+      if constructor.is_a?(Hash)
+        super()
+        update(constructor)
+      else
+        super(constructor)
       end
     end
-    to_json
+
+    # Converts hash into JSON String.
+    # When payload is too long but can be chopped, tries to cut self.[:aps][:alert].
+    # If payload still don't fit Apple's restrictions, returns nil
+    #
+    # @return [String, nil] the object converted into JSON or nil.
+    def to_s
+      # Payload too long
+      if (to_json.length > MAX_PAYLOAD_LEN)
+        alert = self[:aps][:alert]
+        self[:aps][:alert] = ''
+        # can be chopped?
+        if (to_json.length > MAX_PAYLOAD_LEN)
+          return nil
+        else # inefficient way, but payload may be full of unicode-escaped chars, so...
+          self[:aps][:alert] = alert
+          while (self.to_json.length > MAX_PAYLOAD_LEN)
+            self[:aps][:alert].chop!
+          end
+        end
+      end
+      to_json
+    end
+
+    # Invokes {PayloadHash#to_s} and returns it's length
+    # @return [Fixnum, nil] length of object converted into JSON or nil.
+    def length
+      p = to_s
+      p ? p.length : nil
+    end
+
   end
 
-  # Invokes {Hash#to_payload} and returns it's length
-  # @return [Fixnum, nil] length of object converted into JSON or nil.
-  def payload_length
-    p = to_payload
-    p ? p.length : nil
-  end
-
-end
-
-module APNs4r
 
   class Notification
 
@@ -50,7 +60,7 @@ module APNs4r
     # @example
     # APNs4r::Notification.create 'e754dXXXX...', { :aps => {:alert => "Hey, dude!", :badge => 1}, :custom_data => "asd" }
     def Notification.create(token, payload)
-      Notification.new token.kind_of?(String) ? token.delete(' ') : token.to_s(16) , payload.kind_of?(Hash) ? payload.to_payload : payload
+      Notification.new token.kind_of?(String) ? token.delete(' ') : token.to_s(16) , payload.kind_of?(Hash) ? PayloadHash.new(payload).to_s : payload
     end
 
     # Converts to binary string wich can be writen directly into socket
